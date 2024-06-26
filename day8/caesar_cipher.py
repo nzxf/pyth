450a05df66c88af5e9ae1481fbc011504b7e66f1
nzxf <122195155+nzxf@users.noreply.github.com>
2024-06-27 00:01:10 +0700

    Try to solve caesar cipher

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -2,25 +2,59 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+direction = input("Type 'encode' to encrypt or 'decode' to decrypt: ").lower()
+shift = int(input("Enter your code (1-27): "))
+text = input("Enter your message: ")
 
-input_code = int(input("Enter your code (1-27): "))
-input_phrase = input("Enter your message: ")
+def overflow(int):
+    if int < -25:
+        return (int * -1) % 25
+    elif int < 0:
+        return int * -1
+    elif int <= 25:
+        return int
+    else:
+        return int % 25
 
 
-def overflow (number):
-    if number >= 26:
-        return number%26
-    return number
+def cipher(direction,shift,text):
+    # ENCODE = TURN SHIFT INTO NEGATIVE
+    if direction == "decode":
+        shift = shift * -1
+    # START CHIPERING
+    temp_text_list = []
+    for element in text:
+        # SEPARATE ALPHABET
+        if element.isalpha():
+            letter_index = overflow(alphabet.index(element.lower()) + shift)
+            temp_text_list.append(alphabet[overflow(letter_index)])
+        # SEPARATE SYMBOL & WHITESPACE
+        else:
+            temp_text_list.append(element)
+    return "".join(temp_text_list)
 
-def cipher (letter):
-    current_letter_index = alphabet.index(letter)
-    ciphered_letter = alphabet[overflow(current_letter_index + input_code)]
-    return ciphered_letter
+print(cipher(direction,shift,text))
 
-def phrase_cipher (phrase):
-    temporary_list = []
-    for letter in phrase:
-        temporary_list.append(cipher(letter))
-    return "".join(temporary_list)
+# def overflow (number):
+#     if number >= 25:
+#         return number%25
+#     return number
 
-print(phrase_cipher(input_phrase))
+# def cipher (letter):
+#     if letter.isalpha():
+#         current_letter_index = alphabet.index(letter.lower())
+#         ciphered_letter = alphabet[overflow(current_letter_index + input_code)]
+#         return ciphered_letter
+#     else:
+#         return letter
+
+# def uncipher (letter):
+
+
+# def phrase_cipher (phrase):
+#     temporary_list = []
+#     for letter in phrase:
+#         temporary_list.append(cipher(letter))
+#     return "".join(temporary_list)
+
+# print(phrase_cipher(input_phrase))
---
+++ day8/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+direction = input("Type 'encode' to encrypt or 'decode' to decrypt")
+# input_code = int(input("Enter your code (1-27): "))
+# input_phrase = input("Enter your message: ")
