80658cb5fea95d998a083a73c3ca27fbc43efcef
nzxf <122195155+nzxf@users.noreply.github.com>
2024-06-27 22:34:22 +0700

    Complete caesar cipher program

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -1,60 +1,53 @@
+import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt or 'decode' to decrypt: ").lower()
-shift = int(input("Enter your code (1-27): "))
-text = input("Enter your message: ")
 
 def overflow(int):
+    # BUGGY
     if int < -25:
-        return (int * -1) % 25
+        return (int * -1) % 26
     elif int < 0:
-        return int * -1
+        return int - 25
+    # Work
+    
     elif int <= 25:
         return int
+    # BUGGY
     else:
-        return int % 25
+        return int % 26
 
 
 def cipher(direction,shift,text):
     # ENCODE = TURN SHIFT INTO NEGATIVE
     if direction == "decode":
-        shift = shift * -1
+        shift *= -1
     # START CHIPERING
-    temp_text_list = []
+    temp_text = ""
     for element in text:
         # SEPARATE ALPHABET
         if element.isalpha():
             letter_index = overflow(alphabet.index(element.lower()) + shift)
-            temp_text_list.append(alphabet[overflow(letter_index)])
+            temp_text += alphabet[overflow(letter_index)]
         # SEPARATE SYMBOL & WHITESPACE
         else:
-            temp_text_list.append(element)
-    return "".join(temp_text_list)
+            temp_text += element
+    print("============================================")
+    print(f"The {direction}d result: " + temp_text)
+    print("============================================")
 
-print(cipher(direction,shift,text))
+start_over = True
+print(art.title)
+while start_over:
+    direction = input("Type 'encode' to encrypt or 'decode' to decrypt: ").lower()
+    shift = int(input("Enter your code: "))
+    text = input("Enter your message: ")
+    cipher(direction,shift,text)
 
-# def overflow (number):
-#     if number >= 25:
-#         return number%25
-#     return number
-
-# def cipher (letter):
-#     if letter.isalpha():
-#         current_letter_index = alphabet.index(letter.lower())
-#         ciphered_letter = alphabet[overflow(current_letter_index + input_code)]
-#         return ciphered_letter
-#     else:
-#         return letter
-
-# def uncipher (letter):
-
-
-# def phrase_cipher (phrase):
-#     temporary_list = []
-#     for letter in phrase:
-#         temporary_list.append(cipher(letter))
-#     return "".join(temporary_list)
-
-# print(phrase_cipher(input_phrase))
+    answer = input("Do you want to go again? 'yes' or 'no': ")
+    if answer.lower() == "no":
+        print(art.farewell)
+        start_over = False
+    elif answer.lower() == "yes":
+        start_over = True
